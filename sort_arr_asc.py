0924ba766029d29002767e4aac63d1e3a3c83f85
Shanvi <0805shanvi@gmail.com>
2026-08-04 21:30:32 +0530

    sort array in ascending order

---
+++ sort_arr_asc.py
@@ -0,0 +1,19 @@
+# Number of elements
+n = int(input("Enter number of elements: "))
+
+# Input array
+arr = []
+for i in range(n):
+    num = int(input())
+    arr.append(num)
+
+# Bubble Sort
+for i in range(n):
+    for j in range(n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+# Output
+print("Sorted array:")
+for num in arr:
+    print(num, end=" ")
